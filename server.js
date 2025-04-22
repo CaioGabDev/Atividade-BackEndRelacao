0130d1b21fe9e0df5d1b780e386a347c7be8d88d
Caio Lacerda <caio.g.silva20@aluno.senai.br>
2025-04-22 08:21:36 -0300

    feat: add api-key

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const express = require("express");
 const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes");
 const houseRoutes = require("./src/routes/houseRoutes");
+const reportRoutes = require("./src/routes/reportRoutes");
+const setupSwagger = require('./src/config/swagger'); // Swagger aqui
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+setupSwagger(app); // Ativa o Swagger
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/wizards", wizardRoutes);
 app.use("/api/houses", houseRoutes);
+app.use("/api", reportRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/routes/houseRoutes.js
+++ src/routes/houseRoutes.js
@@ -1,11 +1,122 @@
 const express = require("express");
 const router = express.Router();
 const houseController = require("../controllers/houseController");
+const apiKeyMiddleware = require("../config/apiKey"); // 🔐
+
+router.use(apiKeyMiddleware); // 🔒 Protege todas as rotas
+
+
+/**
+ * @swagger
+ * tags:
+ *  name: Houses
+ *  description: Gerenciamento de casas
+ */
+
+/**
+ * @swagger
+ * /api/houses:
+ *   get:
+ *     summary: Lista todos os itens
+ *     tags: [Houses]
+ *     responses:
+ *       200:
+ *         description: Lista retornada com sucesso
+ */
+
+/**
+ * @swagger
+ * /api/houses/{id}:
+ *   get:
+ *     summary: Busca item por ID
+ *     tags: [Houses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Item encontrado
+ *       404:
+ *         description: Item não encontrado
+ */
+
+/**
+ * @swagger
+ * /api/houses:
+ *   post:
+ *     summary: Cria um novo item
+ *     tags: [Houses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               descricao:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Item criado com sucesso
+ */
+
+/**
+ * @swagger
+ * /api/houses/{id}:
+ *   put:
+ *     summary: Atualiza um item
+ *     tags: [Houses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               descricao:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Item atualizado com sucesso
+ */
+
+
+/**
+ * @swagger
+ * /api/houses/{id}:
+ *   delete:
+ *     summary: Deleta um item
+ *     tags: [Houses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Item deletado com sucesso
+ */
+
+
 
 router.get("/", houseController.getAllHouses);
 router.get("/:id", houseController.getHouse);
 router.post("/", houseController.createHouse);
-router.put("/:id", houseController.updateHouse);
 router.delete("/:id", houseController.deleteHouse);
+router.put("/:id", houseController.updateHouse);
 
 module.exports = router;
